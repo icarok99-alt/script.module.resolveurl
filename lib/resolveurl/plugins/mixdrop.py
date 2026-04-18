57b92b26d7cd7bf54b2efc87266b704f22341fad
icarok99-alt <icarok00@gmail.com>
2026-04-18 12:40:05 -0300

    Add files via upload

--- lib/resolveurl/plugins/mixdrop.py
+++ lib/resolveurl/plugins/mixdrop.py
@@ -5,7 +5,15 @@ import re
 
 class MixDropResolver(ResolveUrl):
     name = 'MixDrop'
-    domains = ['*']
+    domains = [
+        'mixdrop.co', 'mixdrop.to', 'mixdrop.sx', 'mixdrop.bz', 'mixdrop.ch',
+        'mixdrp.co', 'mixdrp.to', 'mixdrop.gl', 'mixdrop.club', 'mixdroop.bz',
+        'mixdroop.co', 'mixdrop.vc', 'mixdrop.ag', 'mdy48tn97.com',
+        'md3b0j6hj.com', 'mdbekjwqa.pw', 'mdfx9dc8n.net', 'mixdropjmk.pw',
+        'mixdrop21.net', 'mixdrop.is', 'mixdrop.si', 'mixdrop23.net', 'mixdrop.nu',
+        'mixdrop.ms', 'mdzsmutpcvykb.net', 'mixdrop.ps', 'mxdrop.to', 'mixdrop.sb',
+        'mixdrop.my', 'm1xdrop.net', 'm1xdrop.com', 'm1xdrop.bz'
+    ]
     pattern = r'(?://|\.)([a-z0-9.-]+\.[a-z]{2,})/(?:e|f)/([a-z0-9]+)'
 
     def get_media_url(self, host, media_id):
