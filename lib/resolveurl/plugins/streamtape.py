54a3e0b23c17be87a9c4c098233a362ca488adad
icarok99-alt <icarok00@gmail.com>
2026-04-18 14:03:06 -0300

    Add files via upload

--- lib/resolveurl/plugins/streamtape.py
+++ lib/resolveurl/plugins/streamtape.py
@@ -1,27 +1,8 @@
-"""
-    Plugin for ResolveURL
-    Copyright (C) 2020 gujal
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
-
-import re
 from resolveurl.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
 from six.moves import urllib_error
-
+import re
 
 class StreamTapeResolver(ResolveUrl):
     name = 'StreamTape'
@@ -33,17 +14,13 @@ class StreamTapeResolver(ResolveUrl):
         'streamtape.xyz', 'tapeblocker.com', 'streamnoads.com', 'tapeadvertisement.com',
         'tapeadsenjoyer.com', 'watchadsontape.com'
     ]
-    pattern = r'(?://|\.)(' \
-              r'(?:s(?:tr)?(?:eam|have)?|tapewith|watchadson)?(?:adblock(?:er|plus)?|antiad|noads)?' \
-              r'(?:ta?p?e?|cloud)?(?:blocker|advertisement|adsenjoyer)?\.' \
-              r'(?:com|cloud|net|pe|site|link|cc|online|fun|cash|to|xyz|org|wiki|club)' \
-              r')/(?:e|v)/([0-9a-zA-Z]+)'
+    pattern = r'(?://|\.)([a-z0-9.-]+\.[a-z]{2,})/(?:e|v)/([0-9a-zA-Z]+)'
 
     def get_media_url(self, host, media_id, subs=False):
         web_url = self.get_url(host, media_id)
         headers = {
             'User-Agent': common.FF_USER_AGENT,
-            'Referer': 'https://{0}/'.format(host)
+            'Referer': f'https://{host}/'
         }
         try:
             r = self.net.http_GET(web_url, headers=headers).content
@@ -53,6 +30,7 @@ class StreamTapeResolver(ResolveUrl):
             else:
                 raise ResolverError('Video deleted or removed.')
             return
+
         src = re.findall(r'''ById\('.+?=\s*(["']//[^;<]+)''', r)
         if src:
             src_url = ''
@@ -68,6 +46,7 @@ class StreamTapeResolver(ResolveUrl):
             src_url += '&stream=1'
             src_url = 'https:' + src_url if src_url.startswith('//') else src_url
             src_url = helpers.get_redirect_url(src_url, headers) + helpers.append_headers(headers)
+
             if subs:
                 subtitles = {}
                 s = re.findall(r'<track\s*label="([^"]+)"\s*src="([^"]+)"\s*kind="captions"', r)
