08bbbc403dee60e493a15ab2356215015307bbc6
icarok99-alt <icarok00@gmail.com>
2026-06-18 07:46:08 -0300

    Fix netcine

--- lib/resolveurl/plugins/netcine.py
+++ lib/resolveurl/plugins/netcine.py
@@ -20,17 +20,49 @@ import re
 from resolveurl.lib import helpers
 from resolveurl import common
 from resolveurl.resolver import ResolveUrl, ResolverError
-
-try:
-    from urllib.parse import urlparse, urljoin
-except ImportError:
-    from urlparse import urlparse, urljoin
+from urllib.parse import urlparse, urljoin
 
 
 class NetcineResolver(ResolveUrl):
     name = 'netcine'
     domains = ['*']
     pattern = r'(?://|\.)(netcine[a-z0-9 -]*\.[a-z]{2,10})(/[^\s"\'<>]*)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def _cookiejar(self):
+        try:
+            return (getattr(self.net, 'cookies', None)
+                    or getattr(self.net, '_cj', None)
+                    or getattr(self.net, 'cookiejar', None)
+                    or getattr(self.net.session, 'cookies', None))
+        except Exception:
+            return None
+
+    def _cookie_header(self, url):
+        jar = self._cookiejar()
+        if not jar:
+            return None
+        pairs = [
+            '{0}={1}'.format(c.name, c.value)
+            for c in jar
+            if getattr(c, 'name', None) and getattr(c, 'value', None)
+        ]
+        return '; '.join(pairs) if pairs else None
+
+    def _finalize_headers(self, stream_url, headers, player_url):
+        final = headers.copy()
+        try:
+            pp = urlparse(player_url)
+            final['Referer'] = '{0}://{1}/'.format(pp.scheme, pp.netloc)
+            final['Origin']  = '{0}://{1}'.format(pp.scheme, pp.netloc)
+        except Exception:
+            pass
+        ck = self._cookie_header(stream_url) or self._cookie_header(player_url)
+        if ck:
+            final['Cookie'] = ck
+        return stream_url + helpers.append_headers(final)
 
     def get_url(self, host, media_id):
         host = host.replace(' ', '')
@@ -99,6 +131,6 @@ class NetcineResolver(ResolveUrl):
             hls_url = m2.group(1).strip()
             if hls_url.startswith('/'):
                 hls_url = player_origin + hls_url
-            return hls_url + helpers.append_headers(player_headers)
+            return self._finalize_headers(hls_url, player_headers, player_url)
 
         raise ResolverError('Video Link Not Found')
