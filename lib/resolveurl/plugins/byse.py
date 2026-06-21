8c373bbbeed884c63f3cc6f5ecb9e888acb2bb92
icarok99-alt <icarok00@gmail.com>
2026-06-21 12:03:50 -0300

    Update byse.py

--- lib/resolveurl/plugins/byse.py
+++ lib/resolveurl/plugins/byse.py
@@ -20,6 +20,7 @@ import json
 import os
 import base64
 import hashlib
+import time
 from random import choice
 from six.moves import urllib_parse
 from resolveurl.lib import helpers
@@ -36,72 +37,7 @@ except Exception:
 
 
 def _get_profile():
-    return choice(_PROFILES)
-
-
-def _re(t, e):
-    return (t << e | t >> (32 - e)) & 0xFFFFFFFF
-
-
-def _ye(t):
-    m = 0xFFFFFFFF
-    t[0] = (t[0] + t[1]) & m
-    t[3] = _re(t[3] ^ t[0], 16)
-    t[2] = (t[2] + t[3]) & m
-    t[1] = _re(t[1] ^ t[2], 12)
-    t[0] = (t[0] + t[1]) & m
-    t[3] = _re(t[3] ^ t[0], 8)
-    t[2] = (t[2] + t[3]) & m
-    t[1] = _re(t[1] ^ t[2], 7)
-
-
-def _gr(t):
-    m = 0xFFFFFFFF
-    e = [1779033703, 3144134277, 1013904242, 2773480762]
-    be, lt, dr, lr, hr = 512, 511, 2, 2654435761, 2246822519
-    for i in t:
-        e[0] = (e[0] + i) & m
-        e[0] = _re(e[0], 7)
-        _ye(e)
-    for _ in range(8):
-        _ye(e)
-    r = [0] * be
-    for i in range(be):
-        _ye(e)
-        r[i] = (e[0] ^ e[2]) & m
-    for i in range(dr):
-        for s in range(be):
-            a = r[s] & lt
-            c = (r[s] + r[a]) & m
-            c = _re(c, 13)
-            c = (c ^ ((r[(s + 1) & lt] * lr) & m)) & m
-            r[s] = c
-            e[0] = (e[0] ^ c) & m
-            _ye(e)
-    n = [0] * 8
-    o = int(be / 8)
-    for i in range(8):
-        _ye(e)
-        s = e[0]
-        a = i * o
-        for c in range(o):
-            d = r[a + c]
-            s = (s + d) & m
-            s = _re(s, 5)
-            s = (s ^ ((d * hr) & m)) & m
-        n[i] = (s ^ e[2]) & m
-    return n
-
-
-def _wr(t):
-    e = 0
-    for r in range(len(t)):
-        n = int(t[r])
-        if n == 0:
-            e += 32
-            continue
-        return e + (32 - n.bit_length())
-    return e
+    return choice(_PROFILES) if _PROFILES else {}
 
 
 class ByseResolver(ResolveUrl):
@@ -130,133 +66,210 @@ class ByseResolver(ResolveUrl):
 
         profile = _get_profile()
         client = profile.get('client', {})
+        ua = profile.get('ua', 'Mozilla/5.0 (Linux; Android 13; SM-G780G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36')
 
         headers = {
-            'User-Agent': profile['ua'],
+            'User-Agent': ua,
             'Accept': 'application/json, text/plain, */*',
-            'Origin': ref[:-1],
+            'Origin': ref.rstrip('/'),
             'Referer': web_url,
             'X-Embed-Origin': urllib_parse.urlparse(web_url).netloc,
             'X-Embed-Referer': web_url,
             'X-Embed-Parent': web_url
         }
 
-        challenge_url = '{0}api/videos/access/challenge'.format(ref)
-        resp = self.net.http_POST(challenge_url, headers=headers, form_data={})
-        challenge = json.loads(resp.content)
+        chal_resp = self.net.http_POST(f'{ref}api/videos/access/challenge', headers=headers, form_data={})
+        challenge = json.loads(chal_resp.content)
 
-        attest_url = '{0}api/videos/access/attest'.format(ref)
-        resp = self.net.http_POST(attest_url, headers=headers, form_data=self.wn(challenge, client), jdata=True)
-        attest = json.loads(resp.content)
+        attest_payload = self.wn(challenge, client)
+        att_resp = self.net.http_POST(f'{ref}api/videos/access/attest', headers=headers, form_data=attest_payload, jdata=True)
+        attest = json.loads(att_resp.content)
+
         fingerprint = {
-            'token': attest['token'],
-            'viewer_id': attest['viewer_id'],
-            'device_id': attest['device_id'],
-            'confidence': attest['confidence'],
+            'token': attest.get('token'),
+            'viewer_id': attest.get('viewer_id'),
+            'device_id': attest.get('device_id'),
+            'confidence': attest.get('confidence')
         }
 
-        headers['Cookie'] = 'byse_viewer_id={}; byse_device_id={}'.format(
-            attest['viewer_id'], attest['device_id']
-        )
+        headers['Cookie'] = f"byse_viewer_id={fingerprint['viewer_id']}; byse_device_id={fingerprint['device_id']}"
 
-        captcha_url = '{0}api/videos/{1}/embed/captcha'.format(ref, media_id)
-        resp = self.net.http_POST(captcha_url, headers=headers, form_data={'fingerprint': fingerprint}, jdata=True)
-        captcha = json.loads(resp.content)
-        solution = self.er(captcha['pow_nonce'], captcha['pow_difficulty'])
-        if solution is None:
-            raise ResolverError('Unable to solve captcha')
+        cap_resp = self.net.http_POST(f'{ref}api/videos/{media_id}/embed/captcha', 
+                                     headers=headers, form_data={'fingerprint': fingerprint}, jdata=True)
+        captcha = json.loads(cap_resp.content)
 
-        verify_url = '{0}api/videos/{1}/embed/captcha/verify'.format(ref, media_id)
-        post_data = {'pow_token': captcha['pow_token'], 'solution': solution, 'fingerprint': fingerprint}
-        resp = self.net.http_POST(verify_url, headers=headers, form_data=post_data, jdata=True)
-        verify = json.loads(resp.content)
-        headers.update({'X-Captcha-Token': verify.get('token')})
+        solution = self.er(captcha.get('pow_nonce'), captcha.get('pow_difficulty'))
+        if not solution:
+            raise ResolverError('PoW timeout')
 
-        playback_url = '{0}api/videos/{1}/embed/playback'.format(ref, media_id)
-        resp = self.net.http_POST(playback_url, headers=headers, form_data={'fingerprint': fingerprint}, jdata=True)
-        data = json.loads(resp.content)
+        verify_data = {
+            'pow_token': captcha.get('pow_token'),
+            'solution': solution,
+            'fingerprint': fingerprint
+        }
+        ver_resp = self.net.http_POST(f'{ref}api/videos/{media_id}/embed/captcha/verify',
+                                     headers=headers, form_data=verify_data, jdata=True)
+        verify = json.loads(ver_resp.content)
+        headers['X-Captcha-Token'] = verify.get('token')
+
+        pb_resp = self.net.http_POST(f'{ref}api/videos/{media_id}/embed/playback',
+                                    headers=headers, form_data={'fingerprint': fingerprint}, jdata=True)
+        data = json.loads(pb_resp.content)
+
+        if 'playback' in data:
+            pd = data['playback']
+            try:
+                iv = self.ft(pd.get('iv'))
+                key = self.xn(pd.get('key_parts'), pd.get('version'))
+                payload = self.ft(pd.get('payload'))
+
+                cipher = python_aesgcm.new(key)
+                decrypted_bytes = cipher.open(iv, payload)
+                sources_data = json.loads(decrypted_bytes.decode('utf-8'))
+
+                sources = sources_data.get('sources', [])
+                if sources:
+                    sources_list = [(s.get('label'), s.get('url')) for s in sources]
+                    uri = helpers.pick_source(helpers.sort_sources_list(sources_list))
+                    if uri.startswith('/'):
+                        uri = urllib_parse.urljoin(ref, uri)
+                    return uri + helpers.append_headers(headers)
+            except Exception:
+                pass
 
         sources = data.get('sources')
         if sources:
-            sources = [(x.get('label'), x.get('url')) for x in sources]
-            uri = helpers.pick_source(helpers.sort_sources_list(sources))
+            sources_list = [(x.get('label'), x.get('url')) for x in sources]
+            uri = helpers.pick_source(helpers.sort_sources_list(sources_list))
             if uri.startswith('/'):
                 uri = urllib_parse.urljoin(ref, uri)
-            url = helpers.get_redirect_url(uri, headers=headers)
-            return url + helpers.append_headers(headers)
-        pd = data.get('playback')
-        if pd:
-            iv = self.ft(pd.get('iv'))
-            key = self.xn(pd.get('key_parts'), pd.get('version'))
-            pl = self.ft(pd.get('payload'))
-            cipher = python_aesgcm.new(key)
-            ct = cipher.open(iv, pl)
-            ct = json.loads(ct.decode('latin-1'))
-            sources = ct.get('sources')
-            if sources:
-                sources = [(x.get('label'), x.get('url')) for x in sources]
-                uri = helpers.pick_source(helpers.sort_sources_list(sources))
-                headers.pop('X-Embed-Parent')
-                if 'X-Captcha-Token' in headers.keys():
-                    headers.pop('X-Captcha-Token')
-                return uri + helpers.append_headers(headers)
+            return uri + helpers.append_headers(headers)
 
         raise ResolverError('Video Link Not Found')
 
     def get_url(self, host, media_id):
-        redirect_domains = ['boosteradx.online', 'byse.sx']
-        if host in redirect_domains:
+        if host in ['boosteradx.online', 'byse.sx']:
             host = 'streamlyplayer.online'
         return self._default_get_url(host, media_id, 'https://{host}/e/{media_id}')
 
     @staticmethod
     def ft(e):
+        if not e: return b''
         t = e.replace('-', '+').replace('_', '/')
         return helpers.b64decode(t, binary=True)
 
     def xn(self, e, v):
+        if not e: return b''
         if v:
             v = int(v)
-            e = [e[v - 1], e[len(e) - v]]
-        t = list(map(self.ft, e))
+            e = [e[v-1], e[-1]]
+        t = [self.ft(x) for x in e]
         return b''.join(t)
 
     @staticmethod
-    def wn(ch, client_data=None):
+    def wn(ch, client_data):
         from resolveurl.lib.ecdsa import SigningKey, NIST256p
-        sk = SigningKey.generate(curve=NIST256p, hashfunc=hashlib.sha256)
-        vk = sk.verifying_key.pubkey.point
-        signature = sk.sign(ch.get('nonce').encode(), hashfunc=hashlib.sha256)
+        sk = SigningKey.generate(curve=NIST256p)
+        vk = sk.verifying_key
+        nonce = ch.get('nonce', '')
+
+        signature = sk.sign(nonce.encode('utf-8'), hashfunc=hashlib.sha256)
+        pub_bytes = vk.to_string()
+
         pub = {
-            'alg': 'ES256',
-            'crv': 'P-256', 'ext': True, 'key_ops': ['verify'], 'kty': 'EC',
-            'x': base64.b64encode(vk.x().to_bytes(32, 'big')).decode().replace('+', '-').replace('/', '_').replace('=', ''),
-            'y': base64.b64encode(vk.y().to_bytes(32, 'big')).decode().replace('+', '-').replace('/', '_').replace('=', ''),
-        }
-        sig = base64.b64encode(signature).decode().replace('+', '-').replace('/', '_').replace('=', '')
-        return {
-            'viewer_id': '',
-            'device_id': '',
-            'challenge_id': ch['challenge_id'],
-            'nonce': ch['nonce'],
-            'signature': sig,
-            'public_key': pub,
-            'client': client_data or {},
-            'storage': {},
-            'attributes': {'entropy': 'high'}
+            "crv": "P-256",
+            "ext": True,
+            "key_ops": ["verify"],
+            "kty": "EC",
+            "x": base64.b64encode(pub_bytes[:32]).decode().replace('+','-').replace('/','_').replace('=',''),
+            "y": base64.b64encode(pub_bytes[32:]).decode().replace('+','-').replace('/','_').replace('=','')
         }
 
-    def er(self, t, e, r=20.0):
-        import time
-        if e <= 0:
-            return '0'
-        prefix = t + ':'
+        sig = base64.b64encode(signature).decode().replace('+','-').replace('/','_').replace('=','')
+
+        return {
+            "viewer_id": "",
+            "device_id": "",
+            "challenge_id": ch.get("challenge_id"),
+            "nonce": nonce,
+            "signature": sig,
+            "public_key": pub,
+            "client": client_data,
+            "storage": {},
+            "attributes": {"entropy": "high"}
+        }
+
+    def er(self, t, e, r=35.0):
+        if e <= 0: return "0"
+        prefix = t + ":"
         start = time.time()
         s = 0
         while True:
-            for _ in range(4096):
+            for _ in range(8192):
                 if _wr(_gr((prefix + str(s)).encode())) >= e:
                     return str(s)
                 s += 1
             if time.time() - start > r:
                 return None
+
+
+def _re(t, e):
+    return (t << e | t >> (32 - e)) & 0xFFFFFFFF
+
+
+def _ye(t):
+    m = 0xFFFFFFFF
+    t[0] = (t[0] + t[1]) & m
+    t[3] = _re(t[3] ^ t[0], 16)
+    t[2] = (t[2] + t[3]) & m
+    t[1] = _re(t[1] ^ t[2], 12)
+    t[0] = (t[0] + t[1]) & m
+    t[3] = _re(t[3] ^ t[0], 8)
+    t[2] = (t[2] + t[3]) & m
+    t[1] = _re(t[1] ^ t[2], 7)
+
+
+def _gr(t):
+    m = 0xFFFFFFFF
+    e = [1779033703, 3144134277, 1013904242, 2773480762]
+    for i in t:
+        e[0] = (e[0] + i) & m
+        e[0] = _re(e[0], 7)
+        _ye(e)
+    for _ in range(8):
+        _ye(e)
+    r = [0] * 512
+    for i in range(512):
+        _ye(e)
+        r[i] = (e[0] ^ e[2]) & m
+    for _ in range(2):
+        for s in range(512):
+            a = r[s] & 511
+            c = (r[s] + r[a]) & m
+            c = _re(c, 13)
+            c = (c ^ ((r[(s + 1) & 511] * 2654435761) & m)) & m
+            r[s] = c
+            e[0] = (e[0] ^ c) & m
+            _ye(e)
+    n = [0] * 8
+    for i in range(8):
+        _ye(e)
+        s = e[0]
+        for c in range(64):
+            d = r[i*64 + c]
+            s = (s + d) & m
+            s = _re(s, 5)
+            s = (s ^ ((d * 2246822519) & m)) & m
+        n[i] = (s ^ e[2]) & m
+    return n
+
+
+def _wr(t):
+    e = 0
+    for r in t:
+        n = int(r)
+        if n == 0:
+            e += 32
+            continue
+        return e + (32 - n.bit_length())
+    return e
