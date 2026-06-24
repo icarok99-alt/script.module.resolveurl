ca6e47316c6b4e5e0b96e01da4e0d32a648c288f
icarok99-alt <icarok00@gmail.com>
2026-06-24 19:21:19 -0300

    Update disneycdn.py

--- lib/resolveurl/plugins/disneycdn.py
+++ lib/resolveurl/plugins/disneycdn.py
@@ -24,6 +24,7 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 from resolveurl import common
 from six.moves import urllib_parse
 from resolveurl.lib.pyaes import AESModeOfOperationCBC, Decrypter
+from resolveurl.lib import stream_proxy
 
 class DisneyCDNResolver(ResolveUrl):
     name = 'DisneyCDN'
@@ -36,7 +37,7 @@ class DisneyCDNResolver(ResolveUrl):
 
         referer = f'https://{host}/'
         headers = {
-            'User-Agent': common.FF_USER_AGENT,
+            'User-Agent': common.RAND_UA,
             'Referer': referer,
             'Origin': referer
         }
@@ -101,4 +102,9 @@ class DisneyCDNResolver(ResolveUrl):
             stream_url = urls_found[0]
 
         headers.update({'Origin': referer})
+
+        proxy = stream_proxy.get_proxy()
+        if proxy:
+            return proxy.get_proxy_url(stream_url + helpers.append_headers(headers))
+
         return stream_url + helpers.append_headers(headers)
